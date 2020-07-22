9ecfa7ec65f37fa1782906e8555c1ff4462d05d4
NorbertasL <nlapenas@gmail.com>
2020-07-22 15:11:52 +0100

    Implemented SQLite local storage for daily tasks

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,10 +1,17 @@
+import db.LocalDbConnect;
 import gui.LogInController;
 import javafx.application.Application;
 import javafx.stage.Stage;
 
+
 public class Main extends Application {
     public static void main(String[] args) {
-        launch(args);
+
+        //launch(args);
+        LocalDbConnect.createNewTable(LocalDbConnect.Tables.DAILY);
+        LocalDbConnect.insert(LocalDbConnect.Tables.DAILY,"Read Book 5 min", 30000, "FFFFFF");
+        LocalDbConnect.insert(LocalDbConnect.Tables.DAILY,"Work out 5 min", 344300, "FAA8FF");
+        LocalDbConnect.insert(LocalDbConnect.Tables.DAILY,"Go to bed early", 30000, "00FF00");
     }
 
     @Override
---
+++ src/main/java/db/LocalDbConnect.java
@@ -0,0 +1,60 @@
+package db;
+
+import java.sql.*;
+
+public class LocalDbConnect {
+    private static final String DB_NAME = "tasks.db";
+    private static String url = "jdbc:sqlite:" + DB_NAME;
+
+    public static void connect(){
+        try (Connection conn = DriverManager.getConnection(url)) {
+            if (conn != null) {
+                DatabaseMetaData meta = conn.getMetaData();
+                System.out.println("The driver name is " + meta.getDriverName());
+                System.out.println("A new database has been created.");
+            }
+
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+
+    }
+    public static void createNewTable(Tables table){
+        // SQL statement for creating a new table
+        final String NEW_TABLE = "CREATE TABLE IF NOT EXISTS "+table.name()+" (\n"
+                + "	id integer PRIMARY KEY,\n"
+                + "	task text NOT NULL,\n"
+                + "	alertTime integer NOT NULL,\n"
+                + " colour text NOT NULL\n"
+                + ");";
+        try (Connection conn = DriverManager.getConnection(url);
+             Statement stmt = conn.createStatement()) {
+            // create a new table
+            stmt.execute(NEW_TABLE);
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+    public static void insert(Tables table,String task, int time, String colour) {
+        final String INSERT_SQL = "INSERT INTO "+table.name()+"(task,alertTime,colour) VALUES(?,?,?)";
+
+        try (Connection conn = DriverManager.getConnection(url);
+             PreparedStatement pstmt = conn.prepareStatement(INSERT_SQL)) {
+            if (conn != null) {
+                pstmt.setString(1, task);
+                pstmt.setInt(2, time);
+                pstmt.setString(3, colour);
+                pstmt.executeUpdate();
+            }
+
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    public enum Tables{
+        DAILY,
+        WEEKLY
+    }
+
+}
