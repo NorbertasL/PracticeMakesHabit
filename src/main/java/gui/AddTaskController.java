a133ceb1488d1e6da08547906082563d4dd87347
NorbertasL <nlapenas@gmail.com>
2020-08-04 15:56:19 +0100

    Updated AddTaskController.java

--- src/main/java/gui/AddTaskController.java
+++ src/main/java/gui/AddTaskController.java
@@ -1,4 +1,21 @@
 package gui;
 
-public class AddTaskController {
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class AddTaskController extends Application {
+    Parent root;
+    @Override
+    public void start(Stage stage) throws Exception {
+        root = FXMLLoader.load(getClass().getResource("/fxml/addTask.fxml"));
+        Scene scene = new Scene(root);
+        stage.setResizable(false);
+        stage.setTitle("Add Task");
+        stage.setScene(scene);
+        stage.show();
+
+    }
 }
